116ded62aed95ae22ceeaf498bc5176ee1066f15
Ramnath Vaidyanathan <ramnath@thetaequity.com>
2022-06-03 12:27:53 -0700

    feat: add three numbers

--- R/add.R
+++ R/add.R
@@ -2,13 +2,14 @@
 #'
 #' @param x The first number to add.
 #' @param y  The second number to add.
+#' @param z The third number to add
 #'
 #' @return The sum of two numbers
 #' @export
 #'
 #' @examples
-#' add(1, 2) # 3
-#' add(4, 6) # 10
-add <- function(x, y){
-  x + y
+#' add(1, 2, 3) # 6
+#' add(4, 6, 1) # 11
+add <- function(x, y, z){
+  x + y + z
 }
--- tests/testthat/test-add.R
+++ tests/testthat/test-add.R
@@ -1,3 +1,3 @@
 test_that("adding two numbers works", {
-  expect_equal(add(3, 4), 7)
+  expect_equal(add(3, 4, 1), 8)
 })
